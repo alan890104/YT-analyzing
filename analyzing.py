6a476725143d14534df7cea696ae52c5482f4528
alan890104 <alan890104@gmail.com>
2020-08-06 18:01:56 +0800

    NAME BY TIME

--- analyzing.py
+++ analyzing.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import os
-import openpyxl
+import datetime
+from openpyxl import Workbook
+from openpyxl.chart import BarChart, Series, Reference
+
 import http.client
 import google.oauth2.credentials
 import google_auth_oauthlib.flow
@@ -30,35 +33,56 @@ def get_service():
 def execute_api_request(client_library_function, **kwargs):
     response = client_library_function(**kwargs).execute()
     res = response['rows']
-    print(res)
     return res
 
-def write_excel(res):
-    workbook = openpyxl.Workbook()
+def write_excel(res,today):
+    workbook = Workbook()
     sheet=workbook.active
     title = ['日期','觀看數','觀看時長(小時)','回放時間(秒)','回放比率','獲得訂閱者','失去訂閱者']
+
+    #將資料放入sheet中
     sheet.append(title)
-    for s in res:
-        print(s)
-        sheet.append(s)
-    for xx in ['A','B','C','D','E','F','G']:
-        sheet.column_dimensions[xx].width =15
-    workbook.save('data.xlsx')
+    for s in res:  sheet.append(s)
+    #調整每個欄位的寬度
+    for col in ['A','B','C','D','E','F','G']:
+        sheet.column_dimensions[col].width =15
+    #畫柱狀圖
+    chart = BarChart()
+    chart.type = "col"
+    chart.style = 2
+    chart.title = "youtube數據分析"
+
+    start = datetime.datetime.strptime('2020-03-01',"%Y-%m-%d")
+    days_count = (datetime.datetime.today()-datetime.timedelta(days=3)-start).days
+    data = Reference(sheet,min_row=1, max_row=int(days_count)+2, min_col=2,max_col=7)
+    dates = Reference(sheet, min_row=2,max_row=int(days_count)+2, min_col=1,max_col=1) 
+    chart.add_data(data, titles_from_data=True)
+    chart.x_axis.number_format = "yyyy-mm-dd"
+    chart.set_categories(dates)
+    chart.shape = 10
+    chart.width = 20
+    chart.height = 15
+    sheet.add_chart(chart, "I3")
+    
+    #存成當天日期.xlsx
+    workbook.save(today+'.xlsx')
 
 if __name__ == '__main__':
     # Disable OAuthlib's HTTPs verification when running locally.
     # *DO NOT* leave this option enabled when running in production.
     os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
+    today = str(datetime.datetime.today().strftime("%Y-%m-%d"))
+
     youtubeAnalytics = get_service()
     res = execute_api_request(
         youtubeAnalytics.reports().query,
         ids='channel==MINE',
-        startDate='2020-01-01',
-        endDate='2020-08-02',
+        startDate='2020-03-01',
+        endDate=today,
         metrics='views,estimatedMinutesWatched,averageViewDuration,averageViewPercentage,subscribersGained,subscribersLost',
         dimensions='day',
         sort='day'
     )
-    write_excel(res)
+    write_excel(res,today)
     
